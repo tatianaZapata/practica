8f01fd856c4ccae70fcbace62ba3e106a08e3fd4
Tatiana Zapata Cano - Ceiba Software <tatiana.zapata@ceiba.com.co>
2018-07-05 23:49:23 -0500

    Se crea el proyecto

---
+++ src/main/java/integracionContinua/integracionContinua/IntegracionContinuaApplication.java
@@ -0,0 +1,12 @@
+package integracionContinua.integracionContinua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracionContinuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracionContinuaApplication.class, args);
+	}
+}
